b84c79634bcd5897bcaf0a41be7e7cd87b269b38
Steve Marchant <44552903+mcl-uk@users.noreply.github.com>
2025-05-20 16:34:34 +0100

    Create md5lib.py

---
+++ md5lib.py
@@ -0,0 +1,65 @@
+# MicroPython compatible version of an md5 calculator
+# Use in the same way as haslib's md5 class, with .update() and .digest() methods
+# Also with :bytes input and :bytes output
+# Useful because some hashlibs no longer include md5 functionality
+# Derived from https://github.com/Utkarsh87/md5-hashing
+
+class md5():
+    __rLUT = [7,12,17,22]*4 + [5,9,14,20]*4 + [4,11,16,23]*4 + [6,10,15,21]*4
+    __cLUT = [3614090360, 3905402710,  606105819, 3250441966, 4118548399, 1200080426, 2821735955, 4249261313,
+              1770035416, 2336552879, 4294925233, 2304563134, 1804603682, 4254626195, 2792965006, 1236535329,
+              4129170786, 3225465664,  643717713, 3921069994, 3593408605,   38016083, 3634488961, 3889429448,
+               568446438, 3275163606, 4107603335, 1163531501, 2850285829, 4243563512, 1735328473, 2368359562,
+              4294588738, 2272392833, 1839030562, 4259657740, 2763975236, 1272893353, 4139469664, 3200236656,
+               681279174, 3936430074, 3572445317,   76029189, 3654602809, 3873151461,  530742520, 3299628645,
+              4096336452, 1126891415, 2878612391, 4237533241, 1700485571, 2399980690, 4293915773, 2240044497,
+              1873313359, 4264355552, 2734768916, 1309151649, 4149444226, 3174756917,  718787259, 3951481745]
+    #
+    def __init__(self, msg:bytes=b'') -> None:
+        self.buff = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476]
+        self.blkCnt = 0
+        self.msgTail = msg
+    #
+    def __rl(self, x, cnt):
+        x &= 0xFFFFFFFF
+        return (x << cnt | x >> (32-cnt)) & 0xFFFFFFFF
+    #
+    def __proc64(self, blk64:bytes):
+        #
+        assert(len(blk64) == 64)
+        A,B,C,D = self.buff
+        for i in range(64):
+            if   i//16 == 0: f1 = lambda b,c,d: (b & c) | (~b & d) ; f2 = lambda i: i
+            elif i//16 == 1: f1 = lambda b,c,d: (d & b) | (~d & c) ; f2 = lambda i: (5*i + 1)%16
+            elif i//16 == 2: f1 = lambda b,c,d: b ^ c ^ d ;          f2 = lambda i: (3*i + 5)%16
+            elif i//16 == 3: f1 = lambda b,c,d: c ^ (b | ~d) ;       f2 = lambda i: (7*i)%16
+            F,G = f1(B, C, D), f2(i)
+            rot = A + F + self.__cLUT[i] + int.from_bytes(blk64[4*G : 4*G + 4], 'little')
+            newB = (B + self.__rl(rot, self.__rLUT[i])) & 0xFFFFFFFF
+            A,B,C,D = D,newB,B,C
+        for i, val in enumerate([A, B, C, D]): self.buff[i] = (self.buff[i] + val) & 0xFFFFFFFF
+        return
+    #
+    def update(self, msgBytes) -> None:
+        # add bytes to the hash...
+        self.msgTail += msgBytes
+        while len(self.msgTail) >= 64:
+            self.__proc64(self.msgTail[:64])
+            self.msgTail = self.msgTail[64:]
+            self.blkCnt += 1
+    #
+    def digest(self) -> bytes:
+        # tidy-up, add msgLen return the hash
+        msg_len_in_bits = (8*(len(self.msgTail) + 64*self.blkCnt)) & 0xffffffffffffffff
+        msg = self.msgTail + b'\x80'
+        while len(msg)%64 != 56: msg += b'\x00'
+        while len(msg) >= 64:
+            self.__proc64(msg[:64])
+            msg = msg[64:]
+        msg += msg_len_in_bits.to_bytes(8, 'little')
+        self.__proc64(msg)
+        hash = sum(val<<(32*i) for i, val in enumerate(self.buff))
+        msg = b''
+        self.blkCnt = 0
+        self.msgTail = b''
+        return hash.to_bytes(16,'little')
